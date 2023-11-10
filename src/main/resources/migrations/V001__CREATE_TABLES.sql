7f9fced2d6b9ff70808a802371f5fe43b2a7cc44
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2023-11-10 11:03:54 -0300

    feat banco de dados

--- src/main/resources/migrations/V001__CREATE_TABLES.sql
+++ src/main/resources/migrations/V001__CREATE_TABLES.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 10-Nov-2023 às 13:51
+-- Tempo de geração: 10-Nov-2023 às 14:03
 -- Versão do servidor: 8.0.29
 -- versão do PHP: 7.4.9
 
@@ -24,26 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `role`
+-- Estrutura da tabela `roles`
 --
 
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE IF NOT EXISTS `role` (
-                                      `id` int NOT NULL AUTO_INCREMENT,
-                                      `role` varchar(30) NOT NULL,
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+                                       `id` int NOT NULL AUTO_INCREMENT,
+                                       `role` varchar(30) NOT NULL,
     PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `roles`
+--
+
+INSERT INTO `roles` (`id`, `role`) VALUES
+    (1, 'ADMIN');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `user`
+-- Estrutura da tabela `users`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-                                      `id` int NOT NULL AUTO_INCREMENT,
-                                      `name` varchar(80) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+                                       `id` int NOT NULL AUTO_INCREMENT,
+                                       `name` varchar(80) NOT NULL,
     `cpf` varchar(15) NOT NULL,
     `email` varchar(80) NOT NULL,
     `password` varchar(80) NOT NULL,
@@ -53,17 +60,24 @@ CREATE TABLE IF NOT EXISTS `user` (
     `updated` timestamp NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `role` (`role`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `cpf`, `email`, `password`, `role`, `actived`, `created`, `updated`) VALUES
+    (1, 'Guilherme', '019.733.946-85', 'guilherme@gmail.com', '$2a$10$CC1gMizOXge0iV4fLlaX5eePX/iuH5aoQsoBoOUU.dV4gKzKJUsay', 1, 1, '2023-11-10 14:00:10', '2023-11-10 14:01:22');
 
 --
 -- Restrições para despejos de tabelas
 --
 
 --
--- Limitadores para a tabela `user`
+-- Limitadores para a tabela `users`
 --
-ALTER TABLE `user`
-    ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role`) REFERENCES `role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `users`
+    ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
